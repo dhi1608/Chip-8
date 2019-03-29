a17f8403eaa21c7e2c8c2b656d459ca4dd251fbc
Brian Green <mr.brianjgreen@gmail.com>
2019-03-29 09:04:37 -0400

    updated scan codes

--- src/Chip8Ext.cpp
+++ src/Chip8Ext.cpp
@@ -5,36 +5,6 @@
 #include <cmath>
 #include "Chip8Core.h"
 #include "Chip8Ext.h"
-/**
- * This array maps a SDL scancode to the CHIP-8 key that serves as index
- * for the array. For instance, pressing key keys[5] on your keyboard will
- * make your CHIP-8 understand as if you pressed the [5] key. Remember that
- * CHIP-8 uses a 16-key keyboard with keys labeled 0..F.
- */
-char keys[] = {
-    SDL_SCANCODE_Q, // 0
-    SDL_SCANCODE_W, // 1
-    SDL_SCANCODE_E, // 2
-    SDL_SCANCODE_R, // 3
-    SDL_SCANCODE_A, // 4
-    SDL_SCANCODE_S, // 5
-    SDL_SCANCODE_D, // 6
-    SDL_SCANCODE_F, // 7
-    SDL_SCANCODE_Z, // 8
-    SDL_SCANCODE_X, // 9
-    SDL_SCANCODE_C, // A
-    SDL_SCANCODE_V, // B
-    SDL_SCANCODE_1, // C
-    SDL_SCANCODE_2, // D
-    SDL_SCANCODE_3, // E
-    SDL_SCANCODE_4  // F
-};
-
-static SDL_Window* window = NULL;
-
-static SDL_Renderer* renderer = NULL;
-
-static SDL_Texture* texture = NULL;
 
 #define TEXTURE_PIXEL(x, y) (128 * (y) + (x))
 /**
--- src/Chip8Ext.h
+++ src/Chip8Ext.h
@@ -7,6 +7,44 @@
 #include <SDL2/SDL.h>
 #include "Chip8Core.h"
 
+/**
+ * SDL window
+ */
+static SDL_Window* window = NULL;
+/**
+ * SDL renderer
+ */
+static SDL_Renderer* renderer = NULL;
+/**
+ * SDL texture
+ */
+static SDL_Texture* texture = NULL;
+
+/**
+ * This array maps a SDL scancode to the CHIP-8 key that serves as index
+ * for the array. For instance, pressing key keys[5] on your keyboard will
+ * make your CHIP-8 understand as if you pressed the [5] key. Remember that
+ * CHIP-8 uses a 16-key keyboard with keys labeled 0..F.
+ */
+const char keys[] = {
+    SDL_SCANCODE_0, // 0
+    SDL_SCANCODE_1, // 1
+    SDL_SCANCODE_2, // 2
+    SDL_SCANCODE_3, // 3
+    SDL_SCANCODE_4, // 4
+    SDL_SCANCODE_5, // 5
+    SDL_SCANCODE_6, // 6
+    SDL_SCANCODE_7, // 7
+    SDL_SCANCODE_8, // 8
+    SDL_SCANCODE_9, // 9
+    SDL_SCANCODE_A, // A
+    SDL_SCANCODE_B, // B
+    SDL_SCANCODE_C, // C
+    SDL_SCANCODE_D, // D
+    SDL_SCANCODE_E, // E
+    SDL_SCANCODE_F  // F
+};
+
 int initializeContext();
 void removeContext();
 void renderDisplay(State *state);
